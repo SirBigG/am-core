c9a1ad27f821c5286ed56f69a3353b5333e930b0
andrii <gotsandriy@gmail.com>
2015-10-22 16:05:34 +0300

    admin category for user profile

--- apps/main_app/admin.py
+++ apps/main_app/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from django import forms
-from models import Post, Region, Category, PostPhoto
+from .models import Post, Region, Category, PostPhoto
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     fields = ['title', 'post_category', 'date', 'post_images', 'text']
     list_display = ('title', 'post_category', 'date')
 
-#class UserInformationAdmin(admin.ModelAdmin):
-#    list_display = ('profile','location','phone')
 
-#admin.site.register(UserInformation,UserInformationAdmin)
 admin.site.register(Region)
 admin.site.register(Category)
 admin.site.register(PostPhoto)
--- apps/uagent/admin.py
+++ apps/uagent/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import UserInformation
+
 # Register your models here.
+class UserInformationAdmin(admin.ModelAdmin):
+    list_display = ('profile','location','phone')
+
+admin.site.register(UserInformation,UserInformationAdmin)
