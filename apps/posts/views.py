19ad632804b8e108db6709b5316eece6b6c9e290
andrii <gotsandriy@gmail.com>
2015-11-11 18:22:21 +0200

    Add list and detail view for post.

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Post
+
+
+class ListPostView(ListView):
+    model = Post
+    # TODO: list_post.html
+    template_name = 'list_post.html'
+
+
+class PostView(DetailView):
+    model = Post
+    #TODO: post.html
+    template_name = 'post.html'
+
+    def get_context_data(self,**kwargs):
+        context = super(PostView, self).get_context_data(**kwargs)
+        return context
+
+# TODO: url, templates, comments in context for detail
