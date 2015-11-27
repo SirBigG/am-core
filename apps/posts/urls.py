46e80686c9d6fe47a8ff6d1a868595193b2e1155
andrii <gotsandriy@gmail.com>
2015-11-27 16:25:31 +0200

    Add FormMixin with DetailView.

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from . import views
-from views import ListPostView, PostView
+from views import ListPostView, PostDetailView
 
 urlpatterns = [
     url(r'^$', ListPostView.as_view(), name='list_post_view'),
     url(r'^(?P<page>\d+)/$', ListPostView.as_view(), name='list_post_view'),
-    url(r'^(?P<slug>\w+)/(?P<pk>\d+)/$', PostView.as_view(), name='post_view'),
+    url(r'^(?P<slug>\w+)/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post_view'),
 #TODO: view posts with category things  /<category>/<pk>/ and /posts/<category>/ not /posts/
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, FormView, View
+from django.views.generic.detail import SingleObjectMixin
+from django.http import HttpResponseForbidden
 
 from .models import Post, Comments
 from .forms import CommentsForm
@@ -11,12 +13,42 @@ class ListPostView(ListView):
     paginate_by = 10
 
 
-class PostView(DetailView):
+class PostDisplay(DetailView):
     model = Post
     template_name = 'posts/post.html'
 
     def get_context_data(self,**kwargs):
-        context = super(PostView, self).get_context_data(**kwargs)
+        context = super(PostDisplay, self).get_context_data(**kwargs)
         context['comments'] = Comments.objects.filter(post=self.object).order_by('-publish_date')
         context['comments_form'] = CommentsForm
         return context
+
+
+class CommentFormView(SingleObjectMixin, FormView):
+    form_class = CommentsForm
+    model = Comments
+    # TODO: create template for form
+    template_name = 'posts/post.html'
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return HttpResponseForbidden()
+        self.object = self.get_object()
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+        return super(CommentFormView, self).post(request, *args, **kwargs)
+    # FIXME: Dont work success url. Do it for POST.
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
+
+class PostDetailView(View):
+
+    def get(self, request, *args, **kwargs):
+        view = PostDisplay.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentFormView.as_view()
+        return view(request, *args, **kwargs)
