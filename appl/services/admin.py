12aba58a214c7c78bf95e5119da0636c4b06b723
andrii <gotsandriy@gmail.com>
2016-10-07 12:53:14 +0300

    Meta admin.

--- appl/services/admin.py
+++ appl/services/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from appl.services.models import Feedback
+from appl.services.models import Feedback, MetaData
+
+from modeltranslation.admin import TranslationAdmin
 
 
 class FeedbackAdmin(admin.ModelAdmin):
@@ -8,3 +10,4 @@ class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('title', 'email')
 
 admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(MetaData, TranslationAdmin)
