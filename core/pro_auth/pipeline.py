65cea90f69ed012536319edaba44ee7131a56ebd
michalych <gotsandriy@gmail.com>
2018-04-16 00:20:01 +0300

    Updated login form.

--- core/pro_auth/pipeline.py
+++ core/pro_auth/pipeline.py
@@ -1,7 +1,5 @@
 from social_core.pipeline.partial import partial
 
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 USER_FIELDS = ['email']
@@ -9,15 +7,6 @@ USER_FIELDS = ['email']
 
 @partial
 def add_user_extra_data(strategy, backend, request, details, *args, **kwargs):
-    # If social user created not write extra data to session
-    # if kwargs.get('user', None):
-    #    return
-    # For existing users association
-    # if backend.strategy.session_get('user_pk'):
-    #    return
-    # if not backend.strategy.session_get('social_form_data'):
-        # For adding extra data for new user creation
-    #    return HttpResponseRedirect(redirect_to=reverse('pro_auth:social-register', args=(backend.name,)))
     return
 
 
