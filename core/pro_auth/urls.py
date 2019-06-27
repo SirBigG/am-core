1c325197fef344c5e59a26d2e71499e3b13978d5
Andrii Hots <gotsandriy@gmail.com>
2019-06-27 07:21:09 +0300

    Removed some unused features. Update icons.

--- core/pro_auth/urls.py
+++ core/pro_auth/urls.py
@@ -2,23 +2,16 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
-from core.pro_auth.views import RegisterView, Login, Logout, UserEmailConfirm, UserPasswordReset, IsAuthenticate, \
-    SocialRegisterView, SocialExistUserLogin
+from core.pro_auth.views import Login, Logout, IsAuthenticate, SocialExistUserLogin
 
 
 app_name = 'pro_auth'
 
 urlpatterns = [
-    url(r'^register/$', RegisterView.as_view(), name='register'),
-    url(r'^register/social/(?P<backend_name>[\w-]+)/$', SocialRegisterView.as_view(), name='social-register'),
     url(r'^register/social/(?P<backend_name>[\w-]+)/login/$', SocialExistUserLogin.as_view(), name='social-user-exist'),
     url(r'^login/$', Login.as_view(), name='login'),
     url(r'^logout/$', Logout.as_view(), name='logout'),
     url(r'^is-authenticate/$', IsAuthenticate.as_view(), name='is_authenticate'),
-    url(r'^confirm/email/(?P<hash>[\w-]+).html$', UserEmailConfirm.as_view(), name='email_confirm'),
-    url(r'^password/(?P<action>confirm)/email/', UserPasswordReset.as_view(), name='password-reset'),
-    url(r'^password/(?P<action>check)/email/(?P<hash>[\w-]+).html$',
-        UserPasswordReset.as_view(), name='password-check'),
     # Rendering index page for all urls starts with /profile/ for personal page.
     url(r'^profile/', login_required(TemplateView.as_view(template_name='personal/personal_index.html')),
         name='personal-index'),
--- core/pro_auth/views.py
+++ core/pro_auth/views.py
@@ -1,72 +1,21 @@
-from __future__ import unicode_literals
-
-import hashlib
-
-from datetime import datetime
-
 from django.views.generic import FormView, View
 from django.contrib.auth import login, logout
-from django.contrib.auth.hashers import make_password
 from django.http import HttpResponseRedirect, Http404, JsonResponse
-from django.contrib.auth.forms import SetPasswordForm
-from django.utils.translation import ugettext_lazy as _
-from django.template.loader import render_to_string
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 
 
-from core.pro_auth.forms import UserCreationForm, EmailConfirmForm, LoginForm
-from core.pro_auth.models import User
-
-
-class RegisterView(FormView):
-    form_class = UserCreationForm
-    template_name = 'pro_auth/register.html'
-    success_url = reverse_lazy('index')
-
-    def form_valid(self, form):
-        user = form.save(commit=False)
-        user.is_active = False
-        user.validation_key = hashlib.sha256(
-            str('new_user_%s_%s' % (user.email, user.phone1)).encode('utf-8')).hexdigest()
-        user.save()
-        user.email_user(_("Confirmation of user email."),
-                        _("Register information"),
-                        "agr@agromega.in.ua",
-                        html_message=render_to_string('pro_auth/confirmation_email.html',
-                                                      {'hash': user.validation_key})
-                        )
-        return super().form_valid(form)
-
-
-class SocialRegisterView(RegisterView):
-    template_name = 'pro_auth/register_social.html'
-
-    def form_valid(self, form):
-        _form_data = dict(password=make_password(form.cleaned_data.pop('password1')),
-                          phone1=str(form.cleaned_data.pop('phone1')),
-                          location_id=form.cleaned_data.pop('location').pk,
-                          email=form.cleaned_data.pop('email'))
-        self.request.session['social_form_data'] = _form_data
-        return HttpResponseRedirect(reverse('social:complete', args=(self.kwargs.get('backend_name'),)))
+from core.pro_auth.forms import LoginForm
 
 
 class Login(FormView):
     form_class = LoginForm
     template_name = 'pro_auth/login.html'
-    ajax_template_name = 'pro_auth/login_form.html'
     success_url = '/'
 
     def form_valid(self, form):
         user = form.get_user()
         login(self.request, user)
-        if self.request.is_ajax():
-            data = {'status': 'ok', 'user': user.pk}
-            return JsonResponse(data=data)
-        else:
-            return super().form_valid(form)
-
-    def get_template_names(self):
-        return [self.ajax_template_name] if self.request.is_ajax() else [self.template_name]
+        return super().form_valid(form)
 
 
 class SocialExistUserLogin(Login):
@@ -90,68 +39,3 @@ class IsAuthenticate(View):
         if request.is_ajax():
             return JsonResponse({'is_authenticate': 1 if request.user.is_authenticated else 0})
         raise Http404
-
-
-class UserEmailConfirm(View):
-
-    def get(self, request, **kwargs):
-        _key = kwargs.get('hash', '')
-        if _key:
-            try:
-                user = User.objects.get(validation_key=_key)
-            except User.DoesNotExist:
-                raise Http404
-            user.is_active = True
-            user.validation_key = ''
-            user.save()
-            login(request, user)
-            return HttpResponseRedirect('/')
-
-
-class UserPasswordReset(FormView):
-    """User changing password logic.
-       Sends email with hash in email if user exists.
-       Checking if hash right and reset password."""
-    def get_template_names(self):
-        if self.kwargs.get('action') == 'confirm':
-            return ['pro_auth/email_confirm_for_pass.html']
-        elif self.kwargs.get('action') == 'check':
-            return ['pro_auth/check_password.html']
-
-    def get_form_class(self):
-        if self.kwargs.get('action') == 'confirm':
-            return EmailConfirmForm
-        elif self.kwargs.get('action') == 'check' and self.kwargs.get('hash', ''):
-            return SetPasswordForm
-
-    def get_form(self, form_class=None):
-        if form_class is None:
-            form_class = self.get_form_class()
-        if form_class == SetPasswordForm and self.kwargs.get('hash', ''):
-            try:
-                _user = User.objects.get(validation_key=self.kwargs.get('hash'))
-            except User.DoesNotExist:
-                raise Http404
-            return SetPasswordForm(user=_user, **self.get_form_kwargs())
-        return form_class(**self.get_form_kwargs())
-
-    def form_valid(self, form):
-        if isinstance(form, EmailConfirmForm):
-            _u = form.get_user()
-            _validation_key = hashlib.sha256(
-                str('pass_reset:%s:%s' % (_u.email, datetime.now())).encode('utf-8')).hexdigest()
-            _u.validation_key = _validation_key
-            _u.save()
-            _u.email_user(_("Confirmation of user email."),
-                          _("Changing password email"),
-                          "agr@agromega.in.ua",
-                          html_message=render_to_string('pro_auth/check_password_email.html',
-                                                        {'hash': _validation_key}))
-            return JsonResponse({'status': 'ok'})
-
-        if isinstance(form, SetPasswordForm):
-            _user = form.save(commit=False)
-            _user.validation_key = ''
-            _user.save()
-            login(self.request, _user)
-            return HttpResponseRedirect('/')
