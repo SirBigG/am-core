557149bb90502c242958e5adf67ce4a5c81355f2
SirBigG <gotsandriy@gmail.com>
2019-12-04 23:34:38 +0200

    Try pwa.

--- settings/settings.py
+++ settings/settings.py
@@ -225,11 +225,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.getenv("STATIC_ROOT") or BASE_DIR + '/static/'
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'pwa')
+]
+
+STATIC_ROOT = os.getenv("STATIC_ROOT", BASE_DIR + '/static/')
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.getenv("MEDIA_ROOT") or os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
 CKEDITOR_UPLOAD_PATH = '/media/ckeditor/'
 
