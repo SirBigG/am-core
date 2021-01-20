2e99cb5d8ea117afa64ed44f960fd9091216d9f8
SirBigG <gotsandriy@gmail.com>
2021-01-20 17:12:55 +0200

    Fixed urls

--- core/adverts/urls.py
+++ core/adverts/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 
 from . import views
 
-
 app_name = 'adverts'
-
 
 urlpatterns = [
     path('', views.AdvertListView.as_view(), name="list"),
-    path('<str:category>/', views.AdvertListView.as_view(), name="adverts-list-categories"),
     path('create/', views.AdvertFormView.as_view(), name='adverts-create'),
+    path('<str:category>/', views.AdvertListView.as_view(), name="adverts-list-categories"),
 ]
