0373d7b6ad1c7699de62ab7bd040a0bad1d89377
SirBigG <gotsandriy@gmail.com>
2021-01-20 17:10:30 +0200

    Fixed adverts category 500.

--- core/adverts/urls.py
+++ core/adverts/urls.py
@@ -8,5 +8,6 @@ app_name = 'adverts'
 
 urlpatterns = [
     path('', views.AdvertListView.as_view(), name="list"),
+    path('<str:category>/', views.AdvertListView.as_view(), name="adverts-list-categories"),
     path('create/', views.AdvertFormView.as_view(), name='adverts-create'),
 ]
