a99a9ca25b6f97097bd8e68e419ded95f255ec5d
Andrii Hots <gotsandriy@gmail.com>
2025-03-02 16:25:13 +0100

    Removed text from feedback form.

--- core/services/models.py
+++ core/services/models.py
@@ -1,23 +1,22 @@
+from ckeditor.fields import RichTextField
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
 
 from core.pro_auth.models import User
 
-from ckeditor.fields import RichTextField
-
 
 class Feedback(models.Model):
-    title = models.CharField(max_length=255, verbose_name=_('Заголовок'))
-    email = models.EmailField(blank=True, null=True, verbose_name=_("Email (необов'язково)"))
-    text = models.TextField(verbose_name=_('Текст'))
-    created = models.DateTimeField(auto_now_add=True, verbose_name=_('Дата створення'))
+    title = models.CharField(max_length=255, verbose_name=_("Тема"))
+    email = models.EmailField(blank=True, null=True, verbose_name=_("Email"))
+    text = models.TextField(verbose_name=_("Текст"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_("Дата створення"))
 
     class Meta:
-        db_table = 'feedback'
-        verbose_name = _('Feedback')
-        verbose_name_plural = _('Feedbacks')
+        db_table = "feedback"
+        verbose_name = _("Feedback")
+        verbose_name_plural = _("Feedbacks")
 
     def __str__(self):
         return self.title
@@ -25,15 +24,16 @@ class Feedback(models.Model):
 
 class MetaData(models.Model):
     """Stores meta data for seo optimizing."""
-    title = models.CharField(max_length=255, verbose_name=_('meta title'))
-    description = models.CharField(max_length=255, verbose_name=_('meta description'))
-    h1 = models.CharField(max_length=255, verbose_name=_('h1 tag'), blank=True, null=True)
-    text = RichTextField(blank=True, null=True, verbose_name=_('meta text'))
+
+    title = models.CharField(max_length=255, verbose_name=_("meta title"))
+    description = models.CharField(max_length=255, verbose_name=_("meta description"))
+    h1 = models.CharField(max_length=255, verbose_name=_("h1 tag"), blank=True, null=True)
+    text = RichTextField(blank=True, null=True, verbose_name=_("meta text"))
 
     class Meta:
-        db_table = 'metadata'
-        verbose_name = _('meta data')
-        verbose_name_plural = _('meta data')
+        db_table = "metadata"
+        verbose_name = _("meta data")
+        verbose_name_plural = _("meta data")
 
     def __str__(self):
         return self.title
@@ -45,16 +45,16 @@ MARKS = [(i, i) for i in range(1, 6)]
 class Reviews(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('Review owner.'))
-    mark = models.IntegerField(choices=MARKS, verbose_name=_('Mark'))
-    description = models.TextField(verbose_name=_('Review description'))
-    date = models.DateTimeField(auto_now_add=True, verbose_name=_('Review date'))
-    created = models.DateTimeField(auto_now_add=True, verbose_name=_('Created'))
+    content_object = GenericForeignKey("content_type", "object_id")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("Review owner."))
+    mark = models.IntegerField(choices=MARKS, verbose_name=_("Mark"))
+    description = models.TextField(verbose_name=_("Review description"))
+    date = models.DateTimeField(auto_now_add=True, verbose_name=_("Review date"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_("Created"))
 
     class Meta:
-        verbose_name = _('Review')
-        verbose_name_plural = _('Reviews')
+        verbose_name = _("Review")
+        verbose_name_plural = _("Reviews")
 
     def __str__(self):
-        return '%i-%s' % (self.mark, self.description)
+        return "%i-%s" % (self.mark, self.description)
