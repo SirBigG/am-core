7e79bc0c747756340491f01bb53f999cc056b1e2
Andrii Hots <gotsandriy@gmail.com>
2026-08-07 18:33:09 +0200

    Fixed migration

---
+++ core/posts/migrations/0033_merge_20260807_0000.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("posts", "0032_post_change_own_post_permission"),
+        ("posts", "0032_post_page_h1_alter_post_title"),
+    ]
+
+    operations = []
